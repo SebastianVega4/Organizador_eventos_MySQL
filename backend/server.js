b4f106cb6fa1e14573b2243701aa6f69a19d65c9
unknown <sebastian.vegar2015@gmail.com>
2025-11-09 22:53:25 -0500

    port local

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const dbConfig = {
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'password',
+    password: process.env.MYSQL_PASSWORD || '1234',
     database: process.env.MYSQL_DATABASE || 'eventos_db',
     waitForConnections: true,
     connectionLimit: 10,
